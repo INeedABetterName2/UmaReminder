aeec903d274d333f7a89f0a361def10898d54eca
INeedABetterName2 <100480422+INeedABetterName2@users.noreply.github.com>
2026-07-06 01:41:26 +0900

    Modify UmaNotify.bat to use toast notifications
    Updated the scheduled task to use a toast notification instead of a message box.

--- UmaNotify.bat
+++ UmaNotify.bat
@@ -1,16 +1,15 @@
 @echo off
 setlocal
 
-:: Calculate the time 50 minutes from now
 for /f %%i in ('powershell -NoProfile -Command "(Get-Date).AddMinutes(50).ToString('HH:mm')"') do set NotifyTime=%%i
 
-:: Create a one-time scheduled task
 schtasks /Create ^
  /TN "UmaReminder" ^
  /SC ONCE ^
  /ST %NotifyTime% ^
- /TR "powershell -WindowStyle Hidden -Command Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('自主トレが完了しました','ウマ娘')" ^
- /F >nul
+ /F ^
+ /RL HIGHEST ^
+ /TR "powershell -NoProfile -WindowStyle Hidden -Command \"Start-Process powershell -ArgumentList '-NoProfile -WindowStyle Hidden -Command \"\"$ErrorActionPreference=Stop; [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType=WindowsRuntime] ^| Out-Null; [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType=WindowsRuntime] ^| Out-Null; $xml = New-Object Windows.Data.Xml.Dom.XmlDocument; $xml.LoadXml('<toast><visual><binding template=''ToastGeneric''><text>ウマ娘</text><text>自主トレが完了しました</text></binding></visual></toast>'); $toast = [Windows.UI.Notifications.ToastNotification]::new($xml); [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('UmaReminder').Show($toast)\"\"' -WindowStyle Hidden\""
 
-echo Reminder scheduled for %NotifyTime%.
-timeout /t 2 >nul
+echo Scheduled for %NotifyTime%
+pause
